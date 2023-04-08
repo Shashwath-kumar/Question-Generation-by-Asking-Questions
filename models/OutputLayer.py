3ea0ed1fda614e06601297db8568070eea0ad2d5
Shashwath-kumar <shashwath457@gmail.com>
2023-04-08 01:43:17 -0400

    bug fixes

--- models/OutputLayer.py
+++ models/OutputLayer.py
@@ -17,11 +17,19 @@ class QuestionAnsweringOutputLayer(nn.Module):
 
         # Predict end indices based on the start index
         best_start_idx = torch.argmax(start_probs, dim=-1, keepdim=True)
+        
+        # mask out the positions before the start index to avoid generating invalid answer spans
+        start_mask = torch.ones_like(attention_mask)
+        start_mask.scatter_(1, best_start_idx, 0)
+        cumulative_mask = torch.cumsum(start_mask, dim=1)
+        invalid_positions_mask = cumulative_mask == 0
+
         best_start_embeddings = contextual_embeddings.gather(1, best_start_idx.unsqueeze(-1).repeat(1, 1, contextual_embeddings.size(-1)))
         concatenated_embeddings = torch.cat((contextual_embeddings, best_start_embeddings.repeat(1, contextual_embeddings.size(1), 1)), dim=-1)
         end_scores = self.end_logits(concatenated_embeddings).squeeze(-1)
+        
         if attention_mask is not None:
-            end_scores = end_scores.masked_fill(~attention_mask, float('-inf'))
+            end_scores = end_scores.masked_fill((~attention_mask) | invalid_positions_mask, float('-inf'))
         end_probs = self.softmax(end_scores)
 
         return start_scores, end_scores, start_probs, end_probs
@@ -32,20 +40,25 @@ class QuestionGenerationOutputLayer(nn.Module):
     def __init__(self, d_model, vocab_size):
         super(QuestionGenerationOutputLayer, self).__init__()
         self.output_layer = nn.Linear(d_model, vocab_size)
+        self.pointer_memory = nn.Linear(d_model, 1)
         self.pointer = nn.Linear(d_model, 1)
         self.softmax = nn.Softmax(dim=-1)
-        self.sigmoid = nn.Sigmoid()
 
     def forward(self, decoder_output, memory, attention_mask=None):
         logits = self.output_layer(decoder_output)
         
         # Maxout pointer mechanism
-        pointer_scores = self.pointer(torch.cat((decoder_output, memory), dim=-1)).squeeze(-1)
-        pointer_scores = pointer_scores.masked_fill(~attention_mask, float('-inf'))
+        memory_pointer_scores = self.pointer_memory(memory).transpose(-1, -2)
+        decoder_pointer_scores = self.pointer(decoder_output)
+        pointer_scores = decoder_pointer_scores + memory_pointer_scores
+        
+        if attention_mask is not None:
+            pointer_scores = pointer_scores.masked_fill(~attention_mask.unsqueeze(1), float('-inf'))
+        
         pointer_probs = self.softmax(pointer_scores)
         
         # Combine logits and pointer probabilities
-        extended_logits = torch.cat((logits, pointer_probs.unsqueeze(-1)), dim=-1)
+        extended_logits = torch.cat((logits, pointer_probs), dim=-1)
         probabilities = self.softmax(extended_logits)
         
         return logits, probabilities
