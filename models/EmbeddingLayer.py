437844b39bca352730275dc542ea5954e0cb541a
Shashwath-kumar <shashwath457@gmail.com>
2023-04-08 01:40:11 -0400

    Removed position embedding since T5 has built in position embeddings. Final embeds / sqrt(dimensions) for scaled dot product

--- models/EmbeddingLayer.py
+++ models/EmbeddingLayer.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import math
 from transformers import T5Model
 
 class EmbeddingLayer(nn.Module):
@@ -9,14 +10,12 @@ class EmbeddingLayer(nn.Module):
         self.t5 = T5Model.from_pretrained(pretrained_t5_name)
         self.word_embedding = self.t5.shared  # Use T5's word embeddings
         self.task_embedding = nn.Embedding(3, d_model)  # Create a new embedding layer for task embeddings
-        self.position_embedding = self.t5.encoder.embed_positions
         self.segment_embedding = nn.Embedding(3, d_model)  # Create a new embedding layer for segment embeddings
 
-    def forward(self, input_ids, task_ids, segment_ids, position_ids=None):
+    def forward(self, input_ids, task_ids, segment_ids):
         word_embeds = self.word_embedding(input_ids)
         task_embeds = self.task_embedding(task_ids)
-        position_embeds = self.position_embedding(input_ids)
         segment_embeds = self.segment_embedding(segment_ids)
 
-        embeds = word_embeds + task_embeds + position_embeds + segment_embeds
+        embeds = word_embeds + task_embeds + segment_embeds / math.sqrt(self.d_model)
         return embeds
